dee7cd06ff9a7fe59852f476b03f3d0824ac150f
so-hard <qq1136339397@gmail.com>
2019-07-07 10:58:37 +0800

    feat: 统计字符串出现次数

---
+++ js/collectReStr.js
@@ -0,0 +1,9 @@
+let collectReStr = (str)=> {
+    let map = new Map();
+    for (const list of str) {
+        map.has(list) ?map.set(list,map.get(list)+1) : map.set(list,1)
+    }
+    return [].concat(...map).join("")
+}
+
+collectReStr("asdfsasf")
